7906f4029a6c8437ddfe3e243705d21f347224d6
Luiz Gabriel Moraes Pinheiro <luizgpinheiro0905@gmail.com>
2024-09-03 20:24:13 -0300

    Implementação de Event Handler e GameLoop básico

--- headers/Game.h
+++ headers/Game.h
@@ -8,20 +8,22 @@ class Game
 {
 public:
     // Construtor
-    Game() : window(nullptr), renderer(nullptr) {}
+    Game() : window(nullptr), renderer(nullptr), G_Running(true) {}
 
     // Destrutor
     ~Game()
     {
-        if(renderer)
+        if (renderer)
         {
             SDL_DestroyRenderer(renderer);
         }
-        if(window)
+        if (window)
         {
             SDL_DestroyWindow(window);
         }
     }
+
+    bool G_Running;
 
     // Inicializa a SDL e configura a janela e o renderizador
     bool init(int initflag, const char* title, int xpos, int ypos, int width, int height, int windowflag)
@@ -54,6 +56,40 @@ public:
         return true;
     }
 
+    // Lida com eventos de entrada
+    void handleEvents()
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                {
+                    std::cout << "SDL_QUIT" << std::endl;
+                    G_Running = false;
+                }
+                case SDL_DISPLAYEVENT:
+                {
+                    std::cout << "SDL_DISPLAYEVENT " << std::endl;
+                }
+                case SDL_WINDOWEVENT:
+                {
+                    std::cout << "SDL_WINDOWEVENT " << std::endl;
+                }
+
+            }
+            // Outros eventos como teclas pressionadas podem ser adicionados aqui
+
+        }
+    }
+
+    // Atualiza o estado do jogo (lógica, física, etc.)
+    void update()
+    {
+        // TODO Lógica do Jogo
+    }
+
     // Renderiza na janela
     void render()
     {
@@ -63,16 +99,15 @@ public:
         // Limpa a tela
         SDL_RenderClear(renderer);
 
+        // TODO MOSTRAR CONTEÚDO
+
         // Exibe o que foi renderizado
         SDL_RenderPresent(renderer);
-
-        // Espera 2 segundos
-        SDL_Delay(2000);
     }
 
 private:
     SDL_Window* window;
-    SDL_Renderer* renderer;
+    SDL_Renderer* renderer;// Indica se o jogo está rodando
 };
 
 #endif //GAME_H
--- main.cpp
+++ main.cpp
@@ -3,32 +3,25 @@
 
 int main(int argc, char* argv[])
 {
-    // Inicializa a variável Global para definir se o programa está rodando
-    bool G_Running = false;
-
     // Inicializa a classe Game
-    Game* game = new Game();
+    Game* gameManager = new Game();
 
     // Inicializa SDL, janela e renderizador
-    if(!(G_Running = game->init(SDL_INIT_VIDEO, "MyWindow", 100, 100, 640, 480, SDL_WINDOW_SHOWN)))
+    if (!gameManager->init(SDL_INIT_VIDEO, "MyWindow", 100, 100, 640, 480, SDL_WINDOW_SHOWN))
     {
         return -1;
     }
 
-    // Renderiza a janela
-    game->render();
-    /*
-        TODO Gameloop:
-            while(G_Running)
-            {
-                game->handleEvents();     // Recebe inputs
-                game->update();           // Calcula(física, lógica etc)
-                game->render();           // Renderiza
-            }
-    */
+    // Loop principal do jogo
+    while (gameManager->G_Running)
+    {
+        gameManager->handleEvents();  // Recebe inputs
+        gameManager->update();        // Calcula(física, lógica etc)
+        gameManager->render();        // Renderiza
+    }
 
     // Libera a memória alocada para o jogo
-    delete game;
+    delete gameManager;
 
     // Finaliza o SDL
     SDL_Quit();
